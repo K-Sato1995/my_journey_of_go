7298f223073a92333716c1ae471c13eb1d902ae1
Katsuki <Katsuki_Sato0130@icloud.com>
2019-03-31 14:51:53 +0900

    Modify channels

--- Channels/channels2.go
+++ Channels/channels2.go
@@ -1,26 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-  ch := make(chan bool)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
 
-  fmt.Println("Start!")
-  go process(1, "A", ch)
-  <-ch
-  go process(1, "B", ch)
-  <-ch
-  fmt.Println("Finish!")
+	fmt.Println("Start!")
+
+	go func() {
+		process(1, "A")
+		ch1 <- true
+	}()
+
+	go func() {
+		process(1, "B")
+		ch2 <- true
+	}()
+
+	<-ch1
+	<-ch2
+	fmt.Println("Finish!")
 }
 
-
-func process(num int, str string, ch chan bool) {
-  for i:= 0; i <= num; i++ {
-    time.Sleep(1 * time.Second)
-    fmt.Println(i, str)
-  }
-  ch <- true
+func process(num int, str string) {
+	for i := 0; i <= num; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(i, str)
+	}
 }
