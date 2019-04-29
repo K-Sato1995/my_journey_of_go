10e1ddb7ab495aaa29dd813e671e19673bdc5edc
Katsuki <Katsuki_Sato0130@icloud.com>
2019-04-29 09:03:52 +0900

    Add more to variables

--- Variables/variables.go
+++ Variables/variables.go
@@ -16,4 +16,13 @@ func main() {
 	str2 := "Go"
 	num4 := 2
 	fmt.Printf("%s: %d\n", str2, num4) //=> Go: 2
+
+	// Declaring mutiple variables at a time
+	var (
+		str3 = "JS"
+		num5 = 4
+		arr  = [2]string{"Go", "Ruby"}
+	)
+	fmt.Printf("%v, %v, %v\n", str3, num5, arr) //=> JS, 4, [Go Ruby]
+	fmt.Printf("%T, %T, %T\n", str3, num5, arr) //=> string, int, [2]string
 }
