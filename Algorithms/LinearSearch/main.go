6e654626a2bdea193f72ddb558a67eb60c4ae586
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-01 07:38:31 +0900

    Add LinearSearch

---
+++ Algorithms/LinearSearch/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  var list = []int { 2, 4, 6, 8 ,10}
+  fmt.Printf("The answer is %v\n", linearSearch(list, 5))
+}
+
+func linearSearch( list[]int, item int) bool {
+  for _, value := range list {
+    if value == item {
+      return true
+    }
+  }
+  return false
+}
