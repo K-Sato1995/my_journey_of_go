e6e8b3b301ca993f2e57e8f6ee32812fc2d5ca87
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-08 19:37:56 +0900

    Add comments to SelectionSort

--- Algorithms/SelectionSort/main.go
+++ Algorithms/SelectionSort/main.go
@@ -11,10 +11,12 @@ func main() {
 func selectionSort(list []int) []int {
 	var listLen = len(list)
 
+	// Loop1
 	for i := 0; i < listLen; i++ {
 		// fmt.Println(i)   // 0 1 2 3 4 5 6
 		var minIndex = i // The index of the minimum value
 
+		// Loop2
 		for j := i; j < listLen; j++ {
 			if list[j] < list[minIndex] {
 				minIndex = j
