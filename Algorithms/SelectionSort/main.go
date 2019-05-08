8d41ab4bd68a743a7e7499f210d79e787cec7236
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-08 19:35:50 +0900

    Add SelectionSort

---
+++ Algorithms/SelectionSort/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var list = []int{3, 5, 1, 2, 8, 6, 4}
+	selectionSort(list)
+	fmt.Println(list)
+}
+
+func selectionSort(list []int) []int {
+	var listLen = len(list)
+
+	for i := 0; i < listLen; i++ {
+		// fmt.Println(i)   // 0 1 2 3 4 5 6
+		var minIndex = i // The index of the minimum value
+
+		for j := i; j < listLen; j++ {
+			if list[j] < list[minIndex] {
+				minIndex = j
+			}
+		}
+		// Swap the value on the far left with the minimum value.
+		list[i], list[minIndex] = list[minIndex], list[i]
+	}
+	return list
+}
