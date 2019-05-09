bd1db73c092e83ee8dccd6d341abb367c04f51ca
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-09 19:46:44 +0900

    Add Binary Search

--- Algorithms/BinarySearch/main.go
+++ Algorithms/BinarySearch/main.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
-func main(){
-  
+func main() {
+	var sortedList = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	result, count := binarySearch(sortedList, 7)
+
+	fmt.Printf("The index of the target is %v\n", result)
+	fmt.Printf("It took %v times to search it.\n", count)
+}
+
+// This is referenced from the answer on stackOverFlow.
+// https://stackoverflow.com/questions/43073681/golang-binary-search
+
+func binarySearch(list []int, target int) (result int, count int) {
+	var middleIndex = len(list) / 2
+	fmt.Println(list)
+	//=> [1 2 3 4 5 6 7 8 9 10]
+	//=> [7 8 9 10]
+	//=> [7 8]
+	//=> [7]
+
+	switch {
+	case len(list) == 0:
+		result = -1 // not found
+	case list[middleIndex] > target:
+		result, count = binarySearch(list[:middleIndex], target)
+		// list[:middleIndex] means you are getting a list of the smaller values in the array
+	case list[middleIndex] < target:
+		result, count = binarySearch(list[middleIndex+1:], target)
+		// list[middleIndex+1:] means you are getting a list of the larger values in the array
+		result += middleIndex + 1
+	default:
+		result = middleIndex
+	}
+	count++
+	return
 }
--- Algorithms/README.md
+++ Algorithms/README.md
@@ -1,9 +1,10 @@
 # Overview
 
-I'm just practicing all the things I learnt from [Let's Learn Algorithms](https://www.calhoun.io/lets-learn-algorithms/) and [GOLANG BASIC](http://www.golangprograms.com/).
-All the credit goes to the creaters of those websites.
+I'm just practicing all the things I learnt from articles listed below.
+All the credit goes to the creaters of those articles.
 
 # References
 
 - [Let's Learn Algorithms](https://www.calhoun.io/lets-learn-algorithms/)
 - [GOLANG BASIC](http://www.golangprograms.com/)
+- [programming(guide)](https://programming.guide/go/binary-search.html)
