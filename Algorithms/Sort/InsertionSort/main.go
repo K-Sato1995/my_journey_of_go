44f321a9978dd6aed403f54454cc076aef4e7be6
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-11 09:22:29 +0900

    Add Insertion Sort

---
+++ Algorithms/Sort/InsertionSort/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var list = []int{3, 5, 1, 2, 8, 6, 4}
+
+	fmt.Println(insertionSort(list)) //=> [1 2 3 4 5 6 8]
+}
+
+func insertionSort(list []int) []int {
+	var listLength = len(list)
+
+	// assume list[0] is already sorted
+	for i := 1; i < listLength; i++ {
+		j := i
+		for j > 0 {
+			// list[j] is the current value
+			// list[j-1] is the index of the sorted value that is one value before the current one.
+			if list[j-1] > list[j] {
+				list[j-1], list[j] = list[j], list[j-1] // swap the values if unsorted value is smaller that the sorted value.
+			}
+			j--
+		}
+	}
+
+	return list
+}
