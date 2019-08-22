d3f5877926c288dee9b43a374cd23926619fd77c
Katsuki <Katsuki_Sato0130@icloud.com>
2019-08-22 19:57:45 +0900

    Add more code to pointers

--- Practices/Pointers/main.go
+++ Practices/Pointers/main.go
@@ -11,4 +11,19 @@ func main() {
 
 	*p = 10
 	fmt.Printf("p=%v num=%v\n", p, num) //=> p=0xc420016078 num=10
+
+	// 配列へのポインタ
+	p2 := &[3]int{1, 2, 3}
+
+	fmt.Println((*p2)[0]) //=> 1
+	fmt.Println((p2)[0])  //=> 1
+
+	for i, v := range p2 {
+		fmt.Println(i, v)
+		/*
+			0 1
+			1 2
+			2 3
+		*/
+	}
 }
